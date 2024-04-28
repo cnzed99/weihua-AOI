0e7c901de38909a296adb9aeaabb701d7e25821a
汤传刚 <tangcg@foxmail.com>
2024-04-28 15:04:09 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace 断面毛刺检测软件
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
