9a6eb76d63202acb808b6fdb35bd49d9c088ccae
汤传刚 <tangcg@foxmail.com>
2024-05-06 21:20:26 +0800

    深色主题

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace 断面毛刺检测软件
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.Window
     {
         public MainWindow()
         {
