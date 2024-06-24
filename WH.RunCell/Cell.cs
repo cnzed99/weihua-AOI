1bf898af8b9134f9be38d42a1f95661095f07805
鲍赞宝 <397608785@qq.com>
2024-06-24 16:10:19 +0800

    增加CellBase和RunCell

---
+++ WH.RunCell/Cell.cs
@@ -0,0 +1,435 @@
+
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
+using WH.RecipeCellRootBase;
+
+namespace WH.RunCell
+{
+    public class Cell : CellRootBase<Cell, CellDetection>
+    {
+        public static int MaxParallelism = 5;
+
+        /// <summary>
+        /// 是否是单张图片，最后一张图片
+        /// 提示 使能按钮操作
+        /// </summary>
+        public bool isOnce { get; set; } = false;
+
+        /// <summary>
+        /// 图像文件
+        /// </summary>
+        public string ImageFile;
+
+        /// <summary>
+        /// 缺陷位置图像
+        /// </summary>
+        public BitmapSource DefectPartImg { get; set; }
+        /// <summary>
+        /// 义乌爱旭的丝网特殊用途 从预处理库中拿图显示
+        /// </summary>
+        public BitmapSource ChangleImgae { get; set; }
+
+        public Cell()
+        {
+            this.CreateTime = DateTime.Now;
+            this.Stopwatch = new Stopwatch();
+            this.Stopwatch.Start();
+        }
+
+        /// <summary>
+        /// 是否是OK产品
+        /// </summary>
+        public bool IsOK { get; set; } = false;
+
+        /// <summary>
+        /// 用来存储是质量OK还是颜色OK [0]=质量 [1]=颜色;
+        /// </summary>
+       // public bool[] DetectionOrColorOK { get; set; } = new bool[2];
+
+        /// <summary>
+        /// 流水号
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        /// 产品ID 
+        /// </summary>
+        public string WaferID { get; set; }
+        /// <summary>
+        /// 当前班次的产品序号
+        /// </summary>
+        public int ProductIndex { get; set; }
+
+        /// <summary>
+        /// 接收信息字典
+        /// </summary>
+        public Dictionary<string, string> OtherInfoRecv { get; set; }
+
+        /// <summary>
+        /// 发送信息字典
+        /// </summary>
+        public Dictionary<string, string> OtherInfoSend { get; set; } = new Dictionary<string, string>();
+
+        ///// <summary>
+        ///// 原图像== Image
+        ///// </summary>
+        //public BitmapSource RealImage => this.Image;
+
+        public BitmapSource DownImage { get; set; }
+
+        public BitmapSource SmallImage { get; set; }
+
+        public double defectSize { get; set; }
+
+        /// <summary>
+        /// 检测开始时间 从收到触发信号开始计时
+        /// </summary>
+        public DateTime BeginVisionTime { get; set; }
+
+        /// <summary>
+        /// 创建Cell的时间
+        /// </summary>
+        public DateTime CreateTime { get; private set; }
+
+        public bool _skipthis = false;
+        private bool _timeOut = false;
+
+        /// <summary>
+        /// 超时
+        /// </summary>
+        public bool TimeOut
+        {
+            get => _timeOut;
+            set
+            {
+                _timeOut = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        private bool _preError;
+
+        /// <summary>
+        /// 预处理报错
+        /// </summary>
+        public bool PreError
+        {
+            get { return _preError; }
+            set
+            {
+                _preError = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        private bool _recipeError;
+
+        /// <summary>
+        /// 配方报错
+        /// </summary>
+        public bool RecipeError
+        {
+            get { return _recipeError; }
+            set
+            {
+                _recipeError = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        private bool _isempty = false;
+
+        /// <summary>
+        /// 空料
+        /// </summary>
+        public bool IsEmpty
+        {
+            get => _isempty;
+            set
+            {
+                _isempty = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        private bool _ismix = false;
+
+        /// <summary>
+        /// 混料
+        /// </summary>
+        public bool IsMix
+        {
+            get => _ismix;
+            set
+            {
+                _ismix = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        private bool _isBurst = false;
+
+        /// <summary>
+        /// 爆板
+        /// </summary>
+        public bool IsBurstBoard
+        {
+            get => _isBurst;
+            set
+            {
+                _isBurst = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 颜色等级
+        /// </summary>
+        // public ColorGradeParamConfig ColorGrade { get; set; }
+
+        /// <summary>
+        /// 颜色等级信号
+        /// </summary>
+        //  public int ColorSignel { get; set; }
+
+        /// <summary>
+        /// 颜色值
+        /// </summary>
+        // public double ColorValue { get; set; }
+
+        //  public int QualitySignal { get; set; }
+
+        /// <summary>
+        /// 缺陷等级颜色
+        /// </summary>
+        //  public Color QualityColor { get; set; } = System.Drawing.Color.White;
+
+        /// <summary>
+        /// 缺陷等级颜色名称
+        /// </summary>
+        //  public string QualityColorStr { get; set; } = "White";
+
+        public string DefectType { get; set; }
+
+        public List<string> DetectedDef { get; set; }
+
+        /// <summary>
+        /// 缺陷等级 越低质量越高
+        /// </summary>
+        public int QualityLevel { get; set; } = 0;
+
+        /// <summary>
+        /// 质量信号
+        /// </summary>
+        public int QualitySignal { get; set; }
+
+        /// <summary>
+        /// 质量等级名称G1 G2
+        /// </summary>
+        public string QualityName { get; set; } = "G1";
+
+        /// <summary>
+        /// 计时
+        /// </summary>
+        public Stopwatch Stopwatch { get; set; }
+
+        private bool _frameLoss = false;
+
+        /// <summary>
+        /// 是否丢帧
+        /// </summary>
+        public bool FrameLoss
+        {
+            get { return _frameLoss; }
+            set
+            {
+                _frameLoss = value;
+                if (value)
+                {
+                    _skipthis = true;
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            if (this.DefectPartImg != null)
+            {
+                if (this.DefectPartImg.CanFreeze)
+                {
+                    this.DefectPartImg.Freeze();
+                }
+            }
+            this.Stopwatch.Stop();
+
+            if (DownImage.CanFreeze)
+            {
+                DownImage.Freeze();
+            }
+            if (true)
+            {
+                SmallImage.Freeze();
+            }
+
+        }
+
+
+        public override Cell Clone()
+        {
+            Cell cell = base.Clone();
+            cell.DefectType = this.DefectType;
+            cell.ID = this.ID;
+            cell.IsOK = this.IsOK;
+            cell.IsEmpty = this.IsEmpty;
+            cell.Image = this.Image?.Clone();
+            cell.DownImage = this.DownImage?.Clone();
+            // cell.QualityColorStr = this.QualityColorStr;
+            cell.SmallImage = this.SmallImage?.Clone();
+            cell.QualityLevel = this.QualityLevel;
+            cell.QualityName = this.QualityName;
+            cell.QualitySignal = this.QualitySignal;
+            //cell.QualityColor = this.QualityColor;
+            //cell.ColorSignel = this.ColorSignel;
+            //cell.ColorGrade = this.ColorGrade?.Clone();
+            //cell.ColorValue = this.ColorValue;
+            cell.GetImageTime = this.GetImageTime;
+            cell.CreateTime = this.CreateTime;
+            cell.PreTime = this.PreTime;
+            cell.FlowTimeSpan = this.FlowTimeSpan;
+            cell.RecipeTime = this.RecipeTime;
+            cell.ProcessTime = this.ProcessTime;
+            cell.FilterTime = this.FilterTime;
+            cell.ShowTime = this.ShowTime;
+            cell.Stopwatch = this.Stopwatch;
+            cell.SaveImgTime = this.SaveImgTime;
+            cell.TwoTrgTimeSpan = this.TwoTrgTimeSpan;
+            cell._skipthis = this._skipthis;
+            cell.IsBurstBoard = this.IsBurstBoard;
+            cell.IsMix = this.IsMix;
+            cell.ImageFile = this.ImageFile;
+            cell.LineName = this.LineName;
+            cell.ProjectName = this.ProjectName;
+            cell.CamSerial = this.CamSerial;
+            cell.ProjGuid = this.ProjGuid;
+            cell.ComGuid = this.ComGuid;
+            // cell.DetectionOrColorOK = this.DetectionOrColorOK;
+            cell.OtherInfoRecv = this.OtherInfoRecv;
+            cell.OtherInfoSend = this.OtherInfoSend;
+            cell.DataBytes = this.DataBytes;
+            cell.WaferID = this.WaferID;
+            cell.ProductIndex = this.ProductIndex;
+            return cell;
+        }
+
+        /// <summary>
+        /// 取图时间
+        /// </summary>
+        public TimeSpan GetImageTime { get; set; }
+
+        /// <summary>
+        /// 执行预处理时间
+        /// </summary>
+        public TimeSpan PreTime { get; set; }
+
+        /// <summary>
+        /// 执行配方时间
+        /// </summary>
+        public TimeSpan RecipeTime { get; set; }
+
+        /// <summary>
+        /// 执行筛选时间
+        /// </summary>
+        public TimeSpan FilterTime { get; set; }
+
+        /// <summary>
+        /// 显示时间
+        /// </summary>
+        public TimeSpan ShowTime { get; set; }
+
+        /// <summary>
+        /// 存图时间
+        /// </summary>
+        public TimeSpan SaveImgTime { get; set; }
+
+        /// <summary>
+        /// 总时间
+        /// </summary>
+        public TimeSpan ProcessTime { get; set; }
+
+        public Dictionary<string, TimeSpan> FlowTimeSpan { get; set; }
+
+        public TimeSpan TwoTrgTimeSpan { get; set; }
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public CancellationTokenSource CancelSource
+        {
+            get => _cancellationTokenSource;
+            set
+            {
+                _cancellationTokenSource = value;
+                _parallelOptions = new ParallelOptions()
+                {
+                    CancellationToken = value.Token,
+                    TaskScheduler = TaskScheduler.Default,
+                    MaxDegreeOfParallelism = MaxParallelism
+                };
+            }
+        }
+
+        private ParallelOptions _parallelOptions;
+
+        public ParallelOptions ParallelOption
+        {
+            get => _parallelOptions;
+        }
+
+        /// <summary>
+        /// 制程绑定的相机序列号
+        /// </summary>
+        public string CamSerial { get; set; }
+
+        /// <summary>
+        /// 制程绑定的通讯
+        /// </summary>
+        public string ComGuid { get; set; }
+
+        /// <summary>
+        /// 制程绑定的唯一ID
+        /// </summary>
+        public string ProjGuid { get; set; }
+        /// <summary>
+        /// 线名称（属于哪条产线）
+        /// </summary>
+        public string LineName { get; set; }
+
+        /// <summary>
+        /// 制程名
+        /// </summary>
+        public string ProjectName { get; set; }
+        /// <summary>
+        /// 接收到的信号数据
+        /// </summary>
+        public byte[] DataBytes { get; set; }
+
+    }
+
+}
