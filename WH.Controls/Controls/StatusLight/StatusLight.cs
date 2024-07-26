883f64cad7c4fb31b8bcd2fb2b26f21227046ad2
TCG <1214145387@qq.com>
2024-07-27 00:24:10 +0800

    WH.Controls增加主题Theme.xaml, StatusDotButton,StatusDotLabel

--- WH.Controls/Controls/StatusLight/StatusLight.cs
+++ WH.Controls/Controls/StatusLight/StatusLight.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using HandyControl.Controls;
 
 namespace WH.Controls
 {
@@ -50,5 +52,49 @@ namespace WH.Controls
         }
 
         public StatusLight() { }
+    }
+
+    public static class StatusDotElement
+    {
+        public static int GetRadius(DependencyObject obj)
+        {
+            return (int)obj.GetValue(RadiusProperty);
+        }
+
+        public static void SetRadius(DependencyObject obj, int value)
+        {
+            obj.SetValue(RadiusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Radius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "Radius",
+                typeof(int),
+                typeof(StatusDotElement),
+                new FrameworkPropertyMetadata(10, FrameworkPropertyMetadataOptions.Inherits)
+            );
+
+        public static Brush GetDotBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(DotBrushProperty);
+        }
+
+        public static void SetDotBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(DotBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for DotBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DotBrushProperty =
+            DependencyProperty.RegisterAttached(
+                "DotBrush",
+                typeof(Brush),
+                typeof(StatusDotElement),
+                new FrameworkPropertyMetadata(
+                    default(Brush),
+                    FrameworkPropertyMetadataOptions.Inherits
+                )
+            );
     }
 }
