e6b3e345105b3b3fccac72d66ba6386af91f38bd
TCG <12141@XIAOXIN>
2024-07-31 15:26:13 +0800

    日志记录 错误日志均记录在默认日志中

--- WH.Entity/AssemblyInfo.cs
+++ WH.Entity/AssemblyInfo.cs
@@ -3,5 +3,6 @@ using System.Windows.Markup;
 
 [assembly: XmlnsDefinition("http://whzn.com/rjkf/2024/xaml", "WH.Entity.Converter")]
 [assembly: XmlnsDefinition("http://whzn.com/rjkf/2024/xaml", "WH.Entity")]
+[assembly: XmlnsDefinition("http://whzn.com/rjkf/2024/xaml", "WH.Entity.LogRecord")]
 
 [assembly: XmlnsPrefix("http://whzn.com/rjkf/2024/xaml", "wh")]
