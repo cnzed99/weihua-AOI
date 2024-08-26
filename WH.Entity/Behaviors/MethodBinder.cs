b701205fda6da216801ef4a8a70d32575b0e7edc
TCG <12141@XIAOXIN>
2024-08-26 19:25:43 +0800

    增加常用触发器和行为

---
+++ WH.Entity/Behaviors/MethodBinder.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WH.Entity.Behaviors
+{
+    /// <summary>
+    /// 20240826 TCG
+    /// 提供方法绑定
+    /// </summary>
+    public class MethodBinder
+    {
+        private static readonly ConcurrentDictionary<
+            Type,
+            ConcurrentDictionary<string, Action<object>>
+        > MethodCacheDictionary =
+            new ConcurrentDictionary<Type, ConcurrentDictionary<string, Action<object>>>();
+
+        private Action<object> _method;
+        private MethodInfo _methodInfo;
+        private string _methodName;
+        private Type _targetObjectType;
+
+        public void Invoke(object targetObject, string methodName)
+        {
+            if (targetObject == null)
+                throw new ArgumentNullException(nameof(targetObject));
+            if (methodName == null)
+                throw new ArgumentNullException(nameof(methodName));
+
+            var newTargetObjectType = targetObject.GetType();
+
+            if (_targetObjectType == newTargetObjectType && _methodName == methodName)
+            {
+                if (_method != null)
+                {
+                    _method(targetObject);
+                    return;
+                }
+
+                if (TryGetCacheFromMethodCacheDictionary(out _method) && _method != null)
+                {
+                    _method(targetObject);
+                    return;
+                }
+
+                if (_methodInfo != null)
+                {
+                    _methodInfo.Invoke(targetObject, new object[] { });
+                    return;
+                }
+            }
+
+            _targetObjectType = newTargetObjectType;
+            _methodName = methodName;
+
+            if (TryGetCacheFromMethodCacheDictionary(out _method) && _method != null)
+            {
+                _method(targetObject);
+                return;
+            }
+
+            _methodInfo = _targetObjectType
+                ?.GetMethods()
+                .FirstOrDefault(method =>
+                    method.Name == methodName
+                    && method.GetParameters().Length == 0
+                    && method.ReturnType == typeof(void)
+                );
+
+            if (_methodInfo == null)
+            {
+                var message = $"{_targetObjectType?.Name} Type of Method {methodName} inVisible.";
+                throw new ArgumentException(message);
+            }
+
+            _methodInfo.Invoke(targetObject, new object[] { });
+
+            var taskArgument = new Tuple<Type, MethodInfo>(_targetObjectType, _methodInfo);
+
+            Task.Factory.StartNew(
+                arg =>
+                {
+                    if (arg == null)
+                        throw new ArgumentNullException(nameof(arg));
+
+                    var taskArg = (Tuple<Type, MethodInfo>)arg;
+                    var paraTarget = Expression.Parameter(typeof(object), "target");
+
+                    var method = Expression
+                        .Lambda<Action<object>>(
+                            Expression.Call(
+                                Expression.Convert(
+                                    paraTarget,
+                                    taskArg.Item1 ?? throw new ArgumentException()
+                                ),
+                                taskArg.Item2 ?? throw new ArgumentException()
+                            ),
+                            paraTarget
+                        )
+                        .Compile();
+
+                    var dic =
+                        MethodCacheDictionary.GetOrAdd(
+                            taskArg.Item1,
+                            _ => new ConcurrentDictionary<string, Action<object>>()
+                        ) ?? throw new InvalidOperationException();
+                    dic.TryAdd(taskArg.Item2.Name, method);
+                },
+                taskArgument
+            );
+        }
+
+        private bool TryGetCacheFromMethodCacheDictionary(out Action<object> m)
+        {
+            if (_targetObjectType == null)
+                throw new InvalidOperationException($"{nameof(_targetObjectType)} is null.");
+            if (_methodName == null)
+                throw new InvalidOperationException($"{nameof(_methodName)} is null.");
+
+            m = null;
+            var foundAction = false;
+            if (MethodCacheDictionary.TryGetValue(_targetObjectType, out var actionDictionary))
+                foundAction = actionDictionary.TryGetValue(_methodName, out m);
+
+            return foundAction;
+        }
+    }
+}
---
+++ WH.Entity/Behaviors/WindowCloseCancelBehavior.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using HandyControl.Interactivity;
+
+namespace WH.Entity.Behaviors
+{
+    /// <summary>
+    /// 20240826 TCG
+    /// 窗体取消关闭时执行回调
+    /// </summary>
+    public class WindowCloseCancelBehavior : Behavior<Window>
+    {
+        /// <summary>
+        /// 是否可以关闭
+        /// </summary>
+        public bool CanClose
+        {
+            get { return (bool)GetValue(CanCloseProperty); }
+            set { SetValue(CanCloseProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CanClose.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CanCloseProperty = DependencyProperty.Register(
+            "CanClose",
+            typeof(bool),
+            typeof(WindowCloseCancelBehavior),
+            new PropertyMetadata(true)
+        );
+
+        /// <summary>
+        /// 取消关闭时所执行回调
+        /// </summary>
+        public ICommand CloseCanceledCallbackCommand
+        {
+            get { return (ICommand)GetValue(CloseCanceledCallbackCommandProperty); }
+            set { SetValue(CloseCanceledCallbackCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CloseCanceledCallbackCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CloseCanceledCallbackCommandProperty =
+            DependencyProperty.Register(
+                "CloseCanceledCallbackCommand",
+                typeof(ICommand),
+                typeof(WindowCloseCancelBehavior),
+                new PropertyMetadata(null)
+            );
+
+        public static readonly DependencyProperty CloseCanceledCallbackMethodTargetProperty =
+            DependencyProperty.Register(
+                "CloseCanceledCallbackMethodTarget",
+                typeof(object),
+                typeof(CallMethodAction),
+                new PropertyMetadata(null)
+            );
+
+        // Using a DependencyProperty as the backing store for MethodName.  This enables animation, styling, binding, etc...
+
+        public static readonly DependencyProperty CloseCanceledCallbackMethodNameProperty =
+            DependencyProperty.Register(
+                "CloseCanceledCallbackMethodName",
+                typeof(string),
+                typeof(CallMethodAction),
+                new PropertyMetadata(null)
+            );
+        private readonly MethodBinder _callbackMethod = new MethodBinder();
+        public object CloseCanceledCallbackMethodTarget
+        {
+            get { return GetValue(CloseCanceledCallbackMethodTargetProperty); }
+            set { SetValue(CloseCanceledCallbackMethodTargetProperty, value); }
+        }
+
+        public string CloseCanceledCallbackMethodName
+        {
+            get { return (string)GetValue(CloseCanceledCallbackMethodNameProperty); }
+            set { SetValue(CloseCanceledCallbackMethodNameProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            var associatedObject = AssociatedObject;
+            if (associatedObject == null)
+                throw new InvalidOperationException();
+            base.OnAttached();
+            associatedObject.Closing += (sender, e) =>
+            {
+                if (e == null)
+                    throw new ArgumentNullException(nameof(e));
+
+                if (CanClose)
+                    return;
+                if (
+                    CloseCanceledCallbackCommand != null
+                    && CloseCanceledCallbackCommand.CanExecute(null)
+                )
+                    CloseCanceledCallbackCommand.Execute(null);
+                if (
+                    CloseCanceledCallbackMethodTarget != null
+                    && CloseCanceledCallbackMethodName != null
+                )
+                    _callbackMethod.Invoke(
+                        CloseCanceledCallbackMethodTarget,
+                        CloseCanceledCallbackMethodName
+                    );
+                e.Cancel = true;
+            };
+        }
+    }
+}
